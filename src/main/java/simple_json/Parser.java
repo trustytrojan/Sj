df42c4d74ad42a8c9af50a5b16cfa3ab352a621c
trustytrojan <tarbot24@hotmail.com>
2023-07-06 19:08:25 -0400

    compatible with java 17

--- src/main/java/simple_json/Parser.java
+++ src/main/java/simple_json/Parser.java
@@ -16,69 +16,48 @@ public final class Parser {
 	}
 
 	static Object parse(List<Token> tokens) {
-		if (tokens.size() == 0) {
+		if (tokens.size() == 0)
 			throw new JsonParserException("Tokens list is empty");
-		}
-
 		final var firstToken = tokens.get(0);
-
-		return switch (firstToken) {
-			case final StructuralToken st -> switch (st) {
-				case LEFT_BRACKET -> parseArray(tokens);
-				case LEFT_BRACE -> parseObject(tokens);
-				default -> throw new JsonParserException("what the hell?");
-			};
-
-			case final Value v -> v.value;
-
-			default -> throw new JsonParserException("what the hell?");
-		};
+		if (firstToken == StructuralToken.LEFT_BRACKET)
+			return parseArray(tokens);
+		if (firstToken == StructuralToken.LEFT_BRACE)
+			return parseObject(tokens);
+		if (firstToken instanceof final Value v)
+			return v;
+		throw new JsonParserException("why?");
 	}
 
 	static List<Object> parseArray(List<Token> tokens) {
-		if (!representsArray(tokens)) {
+		if (!representsArray(tokens))
 			throw new JsonParserException("Not a JSON array");
-		}
-
-		if (tokens.size() == 2) {
+		if (tokens.size() == 2)
 			return Collections.emptyList();
-		}
-
 		tokens = withoutFirstAndLastTokens(tokens);
-
 		final var size = tokens.size();
 		final var array = new ArrayList<>();
-
 		for (var i = 0; i < size; ++i) {
 			final var j = indexOfNextNonNestedComma(tokens, i);
 			array.add(parse(tokens.subList(i, j)));
 			i = j;
 		}
-
 		return Collections.unmodifiableList(array);
 	}
 
 	static Map<String, Object> parseObject(List<Token> tokens) {
-		if (!representsObject(tokens)) {
+		if (!representsObject(tokens))
 			throw new JsonParserException("Not a JSON object");
-		}
-
-		if (tokens.size() == 2) {
+		if (tokens.size() == 2)
 			return Collections.emptyMap();
-		}
-
 		tokens = withoutFirstAndLastTokens(tokens);
-
 		final var size = tokens.size();
 		final var map = new HashMap<String, Object>();
-
 		for (var i = 0; i < size; ++i) {
 			final var j = indexOfNextNonNestedComma(tokens, i);
 			final var entry = parseObjectEntry(tokens.subList(i, j));
 			map.put(entry.getKey(), entry.getValue());
 			i = j;
 		}
-
 		return Collections.unmodifiableMap(map);
 	}
 
@@ -110,40 +89,29 @@ public final class Parser {
 		final var size = tokens.size();
 		var depth = 0;
 		for (var i = startIndex; i < size; ++i) {
-			if (depth < 0) {
+			if (depth < 0)
 				throw new JsonParserException("Invalid JSON container: depth < 0");
-			}
-			if (tokens.get(i) instanceof final StructuralToken st) {
+			if (tokens.get(i) instanceof final StructuralToken st)
 				switch (st) {
 					case LEFT_BRACE -> ++depth;
 					case LEFT_BRACKET -> ++depth;
 					case RIGHT_BRACE -> --depth;
 					case RIGHT_BRACKET -> --depth;
-					case COMMA -> {
-						if (depth == 0) {
-							return i;
-						}
-					}
+					case COMMA -> { if (depth == 0) return i; }
 					default -> {}
 				}
-			}
 		}
 		return size;
 	}
 
 	private static Map.Entry<String, Object> parseObjectEntry(List<Token> tokens) {
 		final var size = tokens.size();
-
-		if (size < 3) {
+		if (size < 3)
 			throw new JsonParserException("Invalid JSON object entry: size < 3");
-		}
-
-		if (!(((Value) tokens.get(0)).value instanceof final String key)) {
+		if (!(((Value) tokens.get(0)).value instanceof final String key))
 			throw new JsonParserException("Invalid JSON object entry: key is not a string");
-		} else if (tokens.get(1) != StructuralToken.COLON) {
+		else if (tokens.get(1) != StructuralToken.COLON)
 			throw new JsonParserException("Invalid JSON object entry: no colon present");
-		}
-
 		return Map.entry(key, parse(tokens.subList(2, size)));
 	}
 }
