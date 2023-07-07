722115b7b1fa376db1e31b2876da1c123d775200
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2023-07-07 16:53:39 -0400

    Update Parser.java

--- src/main/java/sj/Parser.java
+++ src/main/java/sj/Parser.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public final class Parser {
+final class Parser {
 	private Parser() {}
 
 	private static class ParserException extends RuntimeException {
